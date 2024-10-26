497299a4a275ac300a2ad2cb67861ac6cdeb1fa2
Bartek <bartek5412@gmail.com>
2024-10-26 10:18:18 +0200

    Tanstack - init

--- src/components/tanstack.tsx
+++ src/components/tanstack.tsx
@@ -1,6 +1,56 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import axios from "axios";
+export const fetchWeather = async () => {
+  const response = await axios.get<WeatherData>(
+    "https://api.open-meteo.com/v1/forecast",
+    {
+      params: {
+        latitude: 52.4069,
+        longitude: 16.9299,
+        hourly: "temperature_2m",
+      },
+    }
+  );
+  return response.data;
+};
+const queryClient = new QueryClient();
+function Weather() {
+  const { data, error, isLoading } = useQuery<WeatherData, Error>({
+    queryKey: ["weather"],
+    queryFn: fetchWeather,
+  });
+  if (isLoading) return <p>Ładowanie danych</p>;
+  if (error) return <p>Bład: {error.message}</p>;
+  const currentHour = new Date().getHours();
+  const currentTemperature = data?.hourly.temperature_2m[currentHour];
+  return (
+    <div>
+      <h2>Progonoza pogody</h2>
+      <p>Aktualna temperatura:</p>
+      <p>
+        Godzina: {currentHour}, temperatura: {currentTemperature}
+      </p>
+    </div>
+  );
+}
+interface WeatherData {
+  hourly: { temperature_2m: number[] };
+}
 
 const Tanstack: React.FC = () => {
-    return <><h1>Tanstack</h1>;</>
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Weather />
+      </QueryClientProvider>
+    </>
+  );
 };
 
 export default Tanstack;
