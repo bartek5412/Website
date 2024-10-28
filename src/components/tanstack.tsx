077dfdb075db098788a7ed2c3bff6828f490f536
Bartek <69474311+bartek5412@users.noreply.github.com>
2024-10-28 11:00:02 +0100

    Time change

--- src/components/tanstack.tsx
+++ src/components/tanstack.tsx
@@ -1,12 +1,11 @@
 import {
   useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 export const fetchWeather = async () => {
   const response = await axios.get<WeatherData>(
     "https://api.open-meteo.com/v1/forecast",
@@ -20,43 +19,78 @@ export const fetchWeather = async () => {
   );
   return response.data;
 };
+
 const queryClient = new QueryClient();
+
 function Weather() {
   const { data, error, isLoading } = useQuery<WeatherData, Error>({
     queryKey: ["weather"],
     queryFn: fetchWeather,
+    refetchInterval: 1000,
   });
 
-  // Stan do przechowywania aktualnej godziny i minut
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState<boolean>(false);
+  const [selectHour, setSelectHour] = useState<number>(new Date().getHours());
 
-  // Aktualizacja godziny i minut co minutę
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 100); // 60000 ms = 1 minuta
+  const toggleSelectedTime = () => {
+    setSelectedTime((prev) => !prev);
+  };
 
-    return () => clearInterval(interval); // Czyści interval po odmontowaniu komponentu
-  }, []);
-  if (isLoading) return <p>Ładowanie danych</p>;
-  if (error) return <p>Bład: {error.message}</p>;
+  const handleHourChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectHour(Number(e.target.value));
+  };
+
+  // Upewnij się, że te warunki są na początku, aby zawsze zwrócić JSX
+  if (isLoading) return <p>Ładowanie danych...</p>;
+  if (error) return <p>Błąd: {error.message}</p>;
+
   const currentHour = new Date().getHours();
   const currentMinutes = new Date().getMinutes();
   const currentSeconds = new Date().getSeconds();
   const currentTemperature = data?.hourly.temperature_2m[currentHour];
   const currentRain = data?.hourly.rain[currentHour];
+  const handleTemperature = data?.hourly.temperature_2m[selectHour];
+  const handleRain = data?.hourly.rain[selectHour];
+
   return (
     <div>
-      <h2>Progonoza pogody</h2>
-      <p>Aktualna temperatura:</p>
-      <p>
-        Godzina: {currentHour}:{currentMinutes}:{currentSeconds}, temperatura:{" "}
-        {currentTemperature}
-      </p>
-      <p>Opady: {currentRain}</p>
+      <button onClick={toggleSelectedTime}>
+        {selectedTime ? "Prognoza godzinowa" : "Aktualna prognoza"}
+      </button>
+
+      {selectedTime ? (
+        <>
+          <h2>Aktualna prognoza</h2>
+
+          <p>Aktualna temperatura:</p>
+          <p>
+            Godzina: {currentHour}:{currentMinutes}:{currentSeconds},
+            temperatura:
+            {currentTemperature}°C
+          </p>
+          <p>Opady: {currentRain} mm</p>
+        </>
+      ) : (
+        <div>
+          {" "}
+          <p>Prognoza godzinowa</p>
+          <p>Wybierz godzinę:</p>
+          <select value={selectHour} onChange={handleHourChange}>
+            {Array.from({ length: 24 }, (_, index) => (
+              <option key={index} value={index}>
+                {index}:00
+              </option>
+            ))}
+          </select>
+          <p>Wybrana godzina: {selectHour}:00</p>
+          <p>Temperatura: {handleTemperature}°C</p>
+          <p>Opady: {handleRain} mm</p>
+        </div>
+      )}
     </div>
   );
 }
+
 interface WeatherData {
   hourly: {
     temperature_2m: number[];
@@ -66,11 +100,9 @@ interface WeatherData {
 
 const Tanstack: React.FC = () => {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <Weather />
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Weather />
+    </QueryClientProvider>
   );
 };
 
